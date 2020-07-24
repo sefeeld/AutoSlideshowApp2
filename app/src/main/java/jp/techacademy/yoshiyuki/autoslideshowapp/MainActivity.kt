f3d157805634965004e10554537e0d1f5985ae87
sefeeld <sefeeld@yahoo.co.jp>
2020-07-24 17:38:41 +0900

    レッスン5課題（7月24日）

--- app/src/main/java/jp/techacademy/yoshiyuki/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yoshiyuki/autoslideshowapp/MainActivity.kt
@@ -23,15 +23,32 @@ class MainActivity : AppCompatActivity() {
             // パーミッションの許可状態を確認する
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 // 許可されている
-                getContentsInfo()
             } else {
                 // 許可されていないので許可ダイアログを表示する
                 requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
             }
             // Android 5系以下の場合
-        } else {
+        }
+
+        // 画像の情報を取得する
+        val resolver = contentResolver
+        val cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+            null, // 項目(null = 全項目)
+            null, // フィルタ条件(null = フィルタなし)
+            null, // フィルタ用パラメータ
+            null // ソート (null ソートなし)
+        )
+
+
+        start_button.setOnClickListener {
             getContentsInfo()
         }
+
+        back_button.setOnClickListener {
+            getContentsInfoback()
+        }
+ //       reset_button.text="test"
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -56,13 +73,56 @@ class MainActivity : AppCompatActivity() {
             null // ソート (null ソートなし)
         )
 
-        if (cursor!!.moveToFirst()) {
-            // indexからIDを取得し、そのIDから画像のURIを取得する
+        if (cursor.moveToNext()) {
+            // 次に進む
+            if(cursor.moveToNext()) {
+                cursor.moveToNext()
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(fieldIndex)
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                imageView.setImageURI(imageUri)
+            }else{
+                //最初に戻る
+                cursor.moveToFirst()
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(fieldIndex)
+                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                imageView.setImageURI(imageUri)
+
+            }
+
+        }
+        cursor.close()
+    }
+
+    private fun getContentsInfoback() {
+        // 画像の情報を取得する
+        val resolver = contentResolver
+        val cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+            null, // 項目(null = 全項目)
+            null, // フィルタ条件(null = フィルタなし)
+            null, // フィルタ用パラメータ
+            null // ソート (null ソートなし)
+        )
+
+        if (cursor!!.moveToPrevious()) {
+            // ひとつ前に戻る
+            cursor.moveToPrevious()
             val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
             val id = cursor.getLong(fieldIndex)
             val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
             imageView.setImageURI(imageUri)
+        }else{
+            //最後に戻る
+            cursor.moveToLast()
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
         }
         cursor.close()
     }
